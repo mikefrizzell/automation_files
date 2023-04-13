6ceb8a97ba9f1efb968f94a2b5cfcec644e2fa0a
Mike Frizzell <mpfrizzell@gmail.com>
2023-04-13 10:36:50 -0500

    Updated to include entire folders

--- backup.py
+++ backup.py
@@ -3,23 +3,36 @@
 import shutil
 import os
 
-# List of files to backup
-files_to_backup = ["file1.txt", "file2.txt", "file3.txt"]
+# List of source folders to backup
+source_folders = ["P:\source_folder_1", "P:\source_folder_2", "P:\source_folder_3"]
 
 # Folder for backup
-backup_folder = "Backup DO NOT DELETE"
+backup_folder = "C:\Users\username\Documents\Backup"
 
-# Create the backup folder if it doesn't exist
-if not os.path.exists(backup_folder):
-    os.mkdir(backup_folder)
-
-# Loop through each file and create a backup
-for file in files_to_backup:
-    # Construct the source and destination file paths
-    src_file = file
-    dest_file = os.path.join(backup_folder, file)
-
-    # Copy the file to the backup folder
-    shutil.copy(src_file, dest_file)
-
-    print(f"File '{file}' backed up to '{dest_file}'")
+# Loop through each source folder in the list
+for source_folder in source_folders:
+    # Extract the folder name from the source folder path
+    folder_name = os.path.basename(source_folder)
+    # Construct destination folder path
+    backup_folder_path = os.path.join(backup_folder, folder_name)
+    # Check if the backup folder already exists, if not create it
+    if not os.path.exists(backup_folder_path):
+        os.mkdir(backup_folder_path)
+    # Loop through all files and folders in the source folder
+    for item in os.listdir(source_folder):
+        source_item = os.path.join(source_folder, item)
+        backup_item = os.path.join(backup_folder_path, item)
+        # Check if the item is a file
+        if os.path.isfile(source_item):
+            # Check if the source file is newer than the backup file
+            if os.path.exists(backup_item) and os.path.getmtime(source_item) <= os.path.getmtime(backup_item):
+                print(f"Skipping '{item}' - No changes detected.")
+            else:
+                # Copy the file to the backup folder
+                shutil.copy(source_item, backup_item)
+                print(f"Backup created: {backup_item}")
+                # Check if the item is a folder
+        elif os.path.isdir(source_item):
+            # Recursively copy the folder and its contents to the backup folder
+            shutil.copytree(source_item, backup_item, dirs_exist_ok=True)
+            print(f"Backup created: {backup_item}")
